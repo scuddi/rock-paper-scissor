2c4dd258cfb465f5b558e586bd8024c8fb5b99b5
Konstantin <157764402+scuddi@users.noreply.github.com>
2024-11-14 14:44:27 +0100

    Add function for the human choice.

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-let resultComputer = ""
+let resultComputer = "";
+
+let resultPerson = "";
 
 function getComputerChoice() {
     let computerNumber = Math.floor(Math.random() * 3 + 1);
     console.log(computerNumber);
 
     if (computerNumber === 1) {
-        resultComputer = "The computer chose Rock"
+        resultComputer = "The computer chose Rock."
         return resultComputer
     } else if (computerNumber === 2) {
-        resultComputer = "The computer chose Paper"
+        resultComputer = "The computer chose Paper."
         return resultComputer
     } else {
-        resultComputer = "The computer chose Scissor"
+        resultComputer = "The computer chose Scissor."
         return resultComputer
     }
 }
 
+function getHumanChoice() {
+    resultPerson = prompt("Are you playing Rock, Paper or Scissor?");
+    resultPerson = resultPerson.toLowerCase();
+
+    if (resultPerson === "rock") {
+        return console.log("You chose Rock.")
+    } else if (resultPerson === "paper") {
+        return console.log("You chose Paper.")
+    } else if (resultPerson === "scissor") {
+        return console.log("You chose Scissor.")
+    } else {
+        return console.log(resultPerson + " is not a possible choice. Choose Rock, Paper or Scissor please.")
+    }
+}
+
 getComputerChoice();
-console.log(resultComputer)
+getHumanChoice();
+console.log(resultComputer);

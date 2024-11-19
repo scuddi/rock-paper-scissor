017192973faf74a4b9f883043eb174e133697192
Konstantin <157764402+scuddi@users.noreply.github.com>
2024-11-19 14:55:05 +0100

    Add functionality to play 5 rounds and count score
    Added for loop to playGame() function as well as
    moved over variables and functions within the big
    playGame function.

--- script.js
+++ script.js
@@ -53,55 +53,57 @@ function getHumanChoice() {
 function playGame() {
     let scoreComputer = 0;
     let scoreHuman = 0;
-    for (let i = 0; i < 4; i++) {
-        function playRound(humanChoice, computerChoice) {
-            if (humanChoice === "rock") {
-                if (computerChoice === "scissor") {
-                    console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-                    scoreHuman = scoreHuman + 1;
-                    return scoreHuman
-                } else if (computerChoice === "paper") {
-                    console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-                    scoreComputer = scoreComputer + 1;
-                    return scoreComputer
-                }
-                else {
-                    return console.log(`It is a draw. Both chose ${humanChoice}.`)
-                }
-            } else if (humanChoice === "scissor") {
-                if (computerChoice === "paper") {
-                    console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-                    scoreHuman = scoreHuman + 1;
-                    return scoreHuman
-                } else if (computerChoice === "rock") {
-                    console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-                    scoreComputer = scoreComputer + 1;
-                    return scoreComputer
-                }
-                else {
-                    return console.log(`It is a draw. Both chose ${humanChoice}.`)
-                }
-            } else {
-                if (computerChoice === "rock") {
-                    console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-                    scoreHuman = scoreHuman + 1;
-                    return scoreHuman
-                } else if (computerChoice === "scissor") {
-                    console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-                    scoreComputer = scoreComputer + 1;
-                    return scoreComputer
-                }
-                else {
-                    return console.log(`It is a draw. Both chose ${humanChoice}.`)
-                };
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === "rock") {
+            if (computerChoice === "scissor") {
+                console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+                scoreHuman = scoreHuman + 1;
+                return scoreHuman
+            } else if (computerChoice === "paper") {
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+                scoreComputer = scoreComputer + 1;
+                return scoreComputer
+            }
+            else {
+                return console.log(`It is a draw. Both chose ${humanChoice}.`)
+            }
+        } else if (humanChoice === "scissor") {
+            if (computerChoice === "paper") {
+                console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+                scoreHuman = scoreHuman + 1;
+                return scoreHuman
+            } else if (computerChoice === "rock") {
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+                scoreComputer = scoreComputer + 1;
+                return scoreComputer
+            }
+            else {
+                return console.log(`It is a draw. Both chose ${humanChoice}.`)
+            }
+        } else {
+            if (computerChoice === "rock") {
+                console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+                scoreHuman = scoreHuman + 1;
+                return scoreHuman
+            } else if (computerChoice === "scissor") {
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+                scoreComputer = scoreComputer + 1;
+                return scoreComputer
+            }
+            else {
+                return console.log(`It is a draw. Both chose ${humanChoice}.`)
             };
         };
     };
-}
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+    //part to play 5 rounds, previous code is unchanged from only playing one round
+    
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+        console.log(`Score of human: ${scoreHuman}; Score of computer: ${scoreComputer}`);
+    };
+};
 
-// playRound(humanSelection, computerSelection);
-
-console.log(`Score of human: ${scoreHuman}; Score of computer: ${scoreComputer}`)
+playGame();
